faa146d8b9643a4a66d90172be9652928cce37e3
veronica-be <verobees@icloud.com>
2026-03-07 12:34:49 -0500

    CSS

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,4 @@
 const currentYear = new Date().getFullYear();
-document.getElementById("currentyear").textContent = currentYear;
+document.getElementById("currentyear").innerHTML = currentYear;
 
-// 2. Get the last modified date/time and output it to the paragraph with id "lastModified"
-document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
+document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
