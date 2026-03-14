e25bea88cee43bbb4ececb8356378f100a758912
veronica-be <verobees@icloud.com>
2026-03-14 15:22:59 -0500

    temple editions

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,13 @@
+/*for the nav on the mobile*/
+const hamButton = document.querySelector('.menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+
 /*for my copyright footer*/
 const currentYear = new Date().getFullYear();
 document.getElementById('currentyear').innerHTML = currentYear;
@@ -5,11 +15,3 @@ document.getElementById('currentyear').innerHTML = currentYear;
 const lastModified = document.lastModified;
 document.getElementById('lastModified').innerHTML = `Last Modified: ${document.lastModified}`;
 
-/*for the nav on the mobile*/
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
-
-hamButton.addEventListener('click', () => {
-    navigation.classList.toggle('open');
-    hamButton.classList.toggle('open');
-});
