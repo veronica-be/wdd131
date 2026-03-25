6e2094ddd49bcf2c3acefdbfbfa344cdddfe631b
veronica-be <verobees@icloud.com>
2026-03-25 09:59:45 -0500

    lazyloading

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,4 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("year").innerHTML = currentYear;
+
+document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
