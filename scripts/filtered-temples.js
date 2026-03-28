918f0c8d6c20d8d32dccdd24917c015b5ac331a6
veronica-be <verobees@icloud.com>
2026-03-28 01:55:10 -0500

    page

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -8,6 +8,120 @@ navButton.addEventListener('click', () => {
 });
 
 
+/* The photo enhancement weel 04*/
+const temples = [
+    {
+        templeName: "Aba Nigeria",
+        location: "Aba, Nigeria",
+        dedicated: "2005, August, 7",
+        area: 11500,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    },
+    {
+        templeName: "Manti Utah",
+        location: "Manti, Utah, United States",
+        dedicated: "1888, May, 21",
+        area: 74792,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    },
+    {
+        templeName: "Payson Utah",
+        location: "Payson, Utah, United States",
+        dedicated: "2015, June, 7",
+        area: 96630,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    },
+    {
+        templeName: "Yigo Guam",
+        location: "Yigo, Guam",
+        dedicated: "2020, May, 2",
+        area: 6861,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    },
+    {
+        templeName: "Washington D.C.",
+        location: "Kensington, Maryland, United States",
+        dedicated: "1974, November, 19",
+        area: 156558,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    },
+    {
+        templeName: "Lima Perú",
+        location: "Lima, Perú",
+        dedicated: "1986, January, 10",
+        area: 9600,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    },
+    {
+        templeName: "Mexico City Mexico",
+        location: "Mexico City, Mexico",
+        dedicated: "1983, December, 2",
+        area: 116642,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    },
+    {
+        templeName: "Rome Italy",
+        location: "Rome, Italy",
+        dedicated: "2019, March, 10",
+        area: 41010,
+        imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/rome-italy-temple/rome-italy-temple-2642-main.jpg"
+    },
+    {
+        templeName: "Hong Kong China",
+        location: "Hong Kong, China",
+        dedicated: "1996, May, 26",
+        area: 51921,
+        imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/hong-kong-china-temple/hong-kong-china-temple-28125-main.jpg"
+    },
+    {
+        templeName: "Seoul Korea",
+        location: "Seoul, Korea",
+        dedicated: "1985, December, 14",
+        area: 28057,
+        imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/seoul-korea-temple/seoul-korea-temple-22305-main.jpg"
+    },
+
+];
+
+/* MAKING CARDS */
+const mainElement = document.querySelector("main");
+
+temples.forEach(temple => {
+    const card = document.createElement(figure)
+    card.innerHTML = `
+            <img 
+                src="${temple.imageUrl}" 
+                alt="${temple.templeName} Temple" 
+                loading="lazy" 
+                width="400" 
+                height="auto"
+            >
+            <figcaption>
+            <h3>${temples.templeName}</h3>
+            <p><span class="label">Location:</span> ${temple.location}</p>
+            <p><span class="label">Dedicated:</span> ${temple.dedicated}</p>
+            <p><span class="label">Area:</span> ${temple.area.toLocaleString()} sq ft</p>
+            </figcaption>
+    `;
+    mainElement.appendChild(card);
+});
+
+<figure>
+    <img src="images/rome-temple.webp" alt="Rome, Italy, Temple at summer evening ">
+        <figcaption>Rome Italy Temple</figcaption>
+</figure>
+
+
 /*for my copyright footer*/
 const currentYear = new Date().getFullYear();
 document.getElementById('currentyear').innerHTML = currentYear;
