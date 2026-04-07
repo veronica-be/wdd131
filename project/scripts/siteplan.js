2e6c1ede4f2798f0bb879fdef17609ec991f4eaa
veronica-be <verobees@icloud.com>
2026-04-06 22:43:07 -0500

    js

--- project/scripts/siteplan.js
+++ project/scripts/siteplan.js
@@ -1,5 +1,5 @@
 const currentYear = new Date().getFullYear();
-document.getElementById('currentyear').innerHTML = currentYear;
+document.getElementById('currentyear').textContent = currentYear;
 
 const lastModified = document.lastModified;
-document.getElementById('lastModified').innerHTML = `Last Modified: ${document.lastModified}`;
+document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
