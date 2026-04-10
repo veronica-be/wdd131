04719d38acc3cd3baa6908d2ea9d40574b73fbf6
veronica-be <verobees@icloud.com>
2026-04-10 12:26:53 -0500

    project

--- project/scripts/confirmation.js
+++ project/scripts/confirmation.js
@@ -1,1 +1,22 @@
-let
+const contactForm = document.getElementById('contactUs');
+
+contactForm.addEventListener('submit', function () {
+    let count = Number(localStorage.getItem('submission-count')) || 0;
+    count += 1;
+    localStorage.setItem('submission-count', count);
+
+    const lastSubmission = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        reason: document.getElementById('reason').value,
+        topic: document.getElementById('topic').value,
+        msg: document.getElementById('message').value
+    };
+
+    localStorage.setItem('last_name', lastSubmission.name);
+    localStorage.setItem('last_email', lastSubmission.email);
+    localStorage.setItem('last_reason', lastSubmission.reason);
+    localStorage.setItem('last_topic', lastSubmission.topic);
+    localStorage.setItem('last_msg', lastSubmission.msg);
+
+});
--- project/scripts/header-footer.js
+++ project/scripts/header-footer.js
@@ -7,3 +7,10 @@ function menuShowMobile(menuMobileX) {
     menuMobileX.classList.toggle("change");
     navigation.classList.toggle('open');
 }
+
+/*FOOTER */
+const currentYear = new Date().getFullYear();
+document.getElementById('currentyear').innerHTML = currentYear;
+
+const lastModified = document.lastModified;
+document.getElementById('lastModified').innerHTML = `Last Modified: ${document.lastModified}`;
