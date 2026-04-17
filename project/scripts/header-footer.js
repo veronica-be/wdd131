c3af94a617ec8a78905fbf8c0b5be9d45dbed49d
veronica-be <verobees@icloud.com>
2026-04-16 21:06:49 -0500

    js

--- project/scripts/header-footer.js
+++ project/scripts/header-footer.js
@@ -1,13 +1,13 @@
 /*HEADER*/
 /*Menu mobile for animation X and show nav*/
 const navigation = document.querySelector('.navigation');
-
+const menuContainer = document.querySelector('.menu-container');
 
 /*function requirement (1 out of 2)*/
-function menuShowMobile(menuMobileX) {
-    menuMobileX.classList.toggle("change");
+menuContainer.addEventListener('click', function () {
+    menuContainer.classList.toggle('change');
     navigation.classList.toggle('open');
-}
+});
 
 /*FOOTER */
 const currentYear = new Date().getFullYear();
